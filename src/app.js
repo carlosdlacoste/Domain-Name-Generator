27009f3ecf105717585d8ad969cfa0ec515dcc60
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-03-21 05:28:34 +0000

    Domain Generator Finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".es", ".net", ".io"];
+
+//1era parte solo con .com
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+    }
+  }
+}
+
+//2da parte con array ext
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+            for(let l = 0; l < ext.length; l++){
+                console.log(pronoun[i] + adj[j] + noun[k] + ext[l]);
+            }
+      }
+    }
+  }
+
+
+//3era parte domain hacks
+// for (let i = 0; i < pronoun.length; i++) {
+//     for (let j = 0; j < adj.length; j++) {
+//         for (let k = 0; k < noun.length; k++) {
+//             for(let l = 0; l < ext.length; l++){
+//                 noun.slice(-1*(ext[1].length-1)) == ext[1].slice(1);
+//                 console.log(pronoun[i] + adj[j] + noun[k] + ext[l]);
+//             }
+//         }
+//     }
+// }
